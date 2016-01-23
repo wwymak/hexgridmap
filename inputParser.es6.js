c974cefa62904f733888027d311cd673e71c17f9
WingYee Mak <wwymak@gmail.com>
2016-01-23 20:03:45 +0000

    some more additions as I try to understand quadtrees

--- inputParser.es6.js
+++ inputParser.es6.js
@@ -9,7 +9,7 @@ var topojsonData = require(__dirname + '/data/uk_counties.topo.json');
 
 var width = 500,
     height = 500;
-var geodata = topojson.feature(data, data.objects.uk_counties)
+var geodata = topojson.feature(topojsonData, topojsonData.objects.uk_counties)
 var projection = resetProjection(geodata);
 var path = d3.geo.path().projection(projection);
 
@@ -47,6 +47,61 @@ var hexblocks = features.map(d => {
     return {
         name: d.properties.NAME,
         cx: centroid[0],
-        cy: centroid[1]
+        cy: centroid[1],
+        clusterX: centroid[0],
+        clusterY: centroid[1]
     }
-})
+});
+
+//moving nodes towards the cluster center
+function gravity(alpha){
+    return (d) => {
+        d.cy += (d.clusterY - d.cy) * alpha;
+        d.cx += (d.clusterX - d.cx) * alpha;
+    };
+}
+
+
+function tick(e) {
+
+}
+
+function collide(alpha){
+    var quadtree = d3.geom.quadtree(hexblocks)
+        .x(function(d){return d.cx;})
+        .y(function(d){return d.cy;})
+
+    var r = node.radius + 16,
+        nx1 = node.x - r,
+        nx2 = node.x + r,
+        ny1 = node.y - r,
+        ny2 = node.y + r;
+    return function(quad, x1, y1, x2, y2) {
+        if (quad.point && (quad.point !== node)) {
+            var x = node.x - quad.point.x,
+                y = node.y - quad.point.y,
+                l = Math.sqrt(x * x + y * y),
+                r = node.radius + quad.point.radius;
+            if (l < r) {
+                l = (l - r) / l * .5;
+                node.x -= x *= l;
+                node.y -= y *= l;
+                quad.point.x += x;
+                quad.point.y += y;
+            }
+        }
+        return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+    };
+}
+
+function onEnd(){
+
+}
+
+var force = d3.layout.force()
+    .nodes(hexblocks)
+    .size([width, height])
+    .gravity(0)
+    .charge(0)
+    .on('tick', tick)
+    .on('end', onEnd);
